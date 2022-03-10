0525c2ca2e02afc74280896861b9044199de979a
jeffjeffmax2 <61031957+jeffjeffmax2@users.noreply.github.com>
2022-03-10 09:28:05 -0800

    Create Disable_Scrolling.js

---
+++ Disable_Scrolling.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Disable Scrolling V2
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @match        https://westmarkschool.learning.powerschool.com/*
+// @match        http://westmarkschool.learning.powerschool.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+     function disableScroll() {
+			document.body.classList.add("stop-scrolling");
+		}
+
+    window.addEventListener("keydown", function(e) {
+    // space, page up, page down and arrow keys:
+    if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
+
+var scrollPosition = [
+  self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+  self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+];
+var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
+html.data('scroll-position', scrollPosition);
+html.data('previous-overflow', html.css('overflow'));
+html.css('overflow', 'hidden');
+window.scrollTo(scrollPosition[0], scrollPosition[1]);
+})();
