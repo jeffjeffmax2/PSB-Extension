f24162bfe69d9c9a2b015958f6a08c84311bbdd8
jeffjeffmax2 <61031957+jeffjeffmax2@users.noreply.github.com>
2022-03-10 10:48:02 -0800

    Update showShameMessage.js

--- showShameMessage.js
+++ showShameMessage.js
@@ -16,3 +16,31 @@ watch.addEventListener('click', function() {
 	document.querySelector('body').removeChild(shame);
 	document.getElementById('appMountPoint').style.display = "block";
 });
+
+
+
+(function() {
+    'use strict';
+
+     function disableScroll() {
+			document.body.classList.add("stop-scrolling");
+		}
+
+    window.addEventListener("keydown", function(e) {
+    // space, page up, page down and arrow keys:
+    if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
+
+var scrollPosition = [
+  self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+  self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+];
+var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
+html.data('scroll-position', scrollPosition);
+html.data('previous-overflow', html.css('overflow'));
+html.css('overflow', 'hidden');
+window.scrollTo(scrollPosition[0], scrollPosition[1]);
+})();
